3cf1de9f5c97ad55a2ca3ba6f21a6515c22db0db
linjj93 <linjj93@hotmail.com>
2019-06-19 19:40:40 +0800

    Practice

--- dataStructure/fixedLengthQueue/fixedLengthQueue.js
+++ dataStructure/fixedLengthQueue/fixedLengthQueue.js
@@ -1,9 +1,36 @@
 module.exports = class FixedLengthQueue {
-  constructor(length) {}
+  constructor(length) {
+    // create array with null values equal to length
+    this.seq = new Array(length);
+    this.nextEnqueueSlot = 0;
+    this.nextDequeueSlot = 0;
+  }
 
-  enqueue(item) {}
+  getSize() {
+    return this.seq.length;
+  }
+  enqueue(item) {
+    if (this.nextEnqueueSlot > this.seq.length - 1) {
+      throw new Error("cannot enqueue");
+    } else {
+      this.seq[this.nextEnqueueSlot] = item;
+      this.nextEnqueueSlot++;
+    }
+  }
 
-  dequeue() {}
+  dequeue(item) {
+    // console.log(this.seq);
+    const result = this.seq[this.nextDequeueSlot];
+    this.seq[this.nextDequeueSlot] = undefined;
+    this.nextDequeueSlot++;
+    return result;
+
+    // console.log(this.seq.length);
+    return result;
+    // console.log(this.seq);
+    // this.seq.pop(item);
+    // console.log(this.seq);
+  }
 
   peek() {}
 };
--- dataStructure/fixedLengthQueue/fixedLengthQueue.test.js
+++ dataStructure/fixedLengthQueue/fixedLengthQueue.test.js
@@ -1,48 +1,48 @@
-const FixedLengthQueue = require('./fixedLengthQueue');
+const FixedLengthQueue = require("./fixedLengthQueue");
 
-describe('FixedLengthQueue', () => {
-  it('should create an array of length passed in', () => {
+describe("FixedLengthQueue", () => {
+  it("should create an array of length passed in", () => {
     const fixedQueue = new FixedLengthQueue(3);
     expect(fixedQueue.getSize()).toEqual(3);
   });
 
-  it('can enqueue and dequeue', () => {
+  it("can enqueue and dequeue", () => {
     const fixedQueue = new FixedLengthQueue(3);
-    fixedQueue.enqueue('apple');
-    expect(fixedQueue.dequeue()).toEqual('apple');
+    fixedQueue.enqueue("apple");
+    expect(fixedQueue.dequeue()).toEqual("apple");
   });
 
-  it('can maintain the order', () => {
+  it("can maintain the order", () => {
     const fixedQueue = new FixedLengthQueue(3);
-    fixedQueue.enqueue('apple');
-    fixedQueue.enqueue('banana');
-    fixedQueue.enqueue('citrus');
-    expect(fixedQueue.dequeue()).toEqual('apple');
-    expect(fixedQueue.dequeue()).toEqual('banana');
-    expect(fixedQueue.dequeue()).toEqual('citrus');
+    fixedQueue.enqueue("apple");
+    fixedQueue.enqueue("banana");
+    fixedQueue.enqueue("citrus");
+    expect(fixedQueue.dequeue()).toEqual("apple");
+    expect(fixedQueue.dequeue()).toEqual("banana");
+    expect(fixedQueue.dequeue()).toEqual("citrus");
     expect(fixedQueue.dequeue()).toEqual(undefined);
   });
 
-  it('cannot insert more than the max length', () => {
+  it("cannot insert more than the max length", () => {
     const fixedQueue = new FixedLengthQueue(2);
-    fixedQueue.enqueue('apple');
-    fixedQueue.enqueue('banana');
-    expect(() => fixedQueue.enqueue('citrus')).toThrow(Error);
+    fixedQueue.enqueue("apple");
+    fixedQueue.enqueue("banana");
+    expect(() => fixedQueue.enqueue("citrus")).toThrow(Error);
   });
 
-  it('cannot insert more than the max length even after dequeue', () => {
+  it("cannot insert more than the max length even after dequeue", () => {
     const fixedQueue = new FixedLengthQueue(2);
-    fixedQueue.enqueue('apple');
-    fixedQueue.enqueue('banana');
+    fixedQueue.enqueue("apple");
+    fixedQueue.enqueue("banana");
     fixedQueue.dequeue();
-    expect(() => fixedQueue.enqueue('citrus')).toThrow(Error);
+    expect(() => fixedQueue.enqueue("citrus")).toThrow(Error);
   });
 
-  it('can peek', () => {
+  it.only("can peek", () => {
     const fixedQueue = new FixedLengthQueue(2);
     expect(fixedQueue.peek()).toEqual(undefined);
-    fixedQueue.enqueue('apple');
-    fixedQueue.enqueue('banana');
-    expect(fixedQueue.peek()).toEqual('apple');
+    fixedQueue.enqueue("apple");
+    fixedQueue.enqueue("banana");
+    expect(fixedQueue.peek()).toEqual("apple");
   });
 });
